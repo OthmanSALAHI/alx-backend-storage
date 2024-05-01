bb006ab3c892c8a8a2a94d50577bb159cf201e3f
Othmansalahi <ootmansalhi.24@gmail.com>
2024-05-01 17:38:57 +0100

    Add Redis cache implementation in exercise.py

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""doc doc module"""
+
+import redis
+import uuid
+
+
+class Cache:
+    """doc doc class"""
+
+    def __init__(self):
+        """doc doc method"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    @count_calls
+    @call_history
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """doc doc method"""
+        keyx = str(uuid.uuid4())
+        self._redis.set(keyx, data)
+        return keyx
+
+    def get(
+        self, key: str, fn: Optional[Callable] = None
+    ) -> Union[str, bytes, int, float]:
+        """doc doc method"""
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """doc doc method"""
+        return self.get(key, fn=str)
+
+    def get_int(self, key: str) -> int:
+        """doc doc method"""
+        return self.get(key, fn=int)
